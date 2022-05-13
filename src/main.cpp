8b12fdbc424169ce34c9451219a839db139cec70
Dextron12 <ethangidman@gmail.com>
2022-05-13 10:26:24 +1000

    Created EBO example

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 #include "Shader.hpp"
 
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f
+    -0.5f, -0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f
+};
+
+unsigned int indicies[] = {
+    //First triangle
+    0, 1, 3,
+    1, 2, 3
 };
 
 std::string dirPath;
@@ -15,14 +22,17 @@ int main(int argc, char** argv){
     t_Window window("Proxy", 800, 600);
     dirPath = _getcwd(NULL, 0);
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO); // Generate EBO buffer
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); //Bind EBO buffer
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -35,8 +45,9 @@ int main(int argc, char** argv){
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 4);
 
         SDL_GL_SwapWindow(window.window);
     }
