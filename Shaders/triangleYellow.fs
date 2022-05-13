fa16b4bf683aa2601f0e6eca8c00f0c75efffaef
Dextron12 <ethangidman@gmail.com>
2022-05-13 14:05:07 +1000

    Two triangles using different VBO's, VAO's & Shader programs.

---
+++ Shaders/triangleYellow.fs
@@ -0,0 +1,7 @@
+#version 430 core
+
+out vec4 FragColor;
+
+void main(){
+    FragColor = vec4(0.0, 0.3, 0.6, 1.0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,17 @@ unsigned int indicies[] = {
     1, 2, 3
 };
 
+float triangle1[] = {
+    -0.75f, -0.25f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+    -0.25f, -0.25f, 0.0f
+};
+float triangle2[] = {
+    0.25f, -0.25f, 0.0f,
+    0.75f, -0.25f, 0.0f,
+    0.5f, 0.25f, 0.0f
+};
+
 std::string dirPath;
 
 
@@ -34,7 +45,7 @@ int main(int argc, char** argv){
     t_Window window("Proxy", 800, 600);
     dirPath = _getcwd(NULL, 0);
 
-    unsigned int VBO, VAO, EBO;
+    /*unsigned int VBO, VAO, EBO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &EBO); // Generate EBO buffer
@@ -47,9 +58,33 @@ int main(int argc, char** argv){
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);*/
+
+    unsigned int triVBO, triVAO;
+    glGenBuffers(1, &triVBO);
+    glGenVertexArrays(1, &triVAO);
+
+    glBindVertexArray(triVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, triVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    unsigned int triVBO1, triVAO1;
+    glGenBuffers(1, &triVBO1);
+    glGenVertexArrays(1, &triVAO1);
+
+    glBindVertexArray(triVAO1);
+    glBindBuffer(GL_ARRAY_BUFFER, triVBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle2), triangle2, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
     Shader shader(dirPath + "/Shaders/vertex.s", dirPath + "/Shaders/fragment.s");
+    Shader yellowTriangle(dirPath + "/Shaders/vertex.s", dirPath + "/Shaders/triangleYellow.fs");
 
     while (window.b_appState){
         window.updateEvents();
@@ -57,12 +92,12 @@ int main(int argc, char** argv){
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
-        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(triVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        //glDrawArrays(GL_TRIANGLES, 0, 4);
+        yellowTriangle.use();
+        glBindVertexArray(triVAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         SDL_GL_SwapWindow(window.window);
     }
